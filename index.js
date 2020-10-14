3d6fcb0cabbbd9961a735bdb896ed4939b18a47c
Anurag Mittal <anurag.mittal@scality.com>
2020-10-15 01:51:25 +0200

    clustering using node module, cluster

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const express = require('express')
-app = express();
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require('cluster');
 
-function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {}
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+    // Cause index.js to be executed *again* but in child mode
+    cluster.fork();
+    cluster.fork();
+} else {
+    // I am a child and I am going to act like a server and do nothing else
+    const crypto = require('crypto');
+    const express = require('express')
+    app = express();
+
+    app.get('/', (req, res) => {
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('Hello World');
+        });
+    })
+
+    app.get('/fast', (req, res) => {
+        res.send('This was fast');
+    })
+
+    app.listen(3000);
 }
 
-app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hello World');
-})
-
-app.listen(3000);
+/**
+ * Apache Benchmarking
+ * ab -c 50 -n 500 localhost:3000/fast
+ * -c => concurrency
+ * -n => number of operations
+ */
